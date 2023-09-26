2e5410ae2fcdf50c3076f4e0a5dee8a6410ead11
Selyss <99344963+Selyss@users.noreply.github.com>
2023-09-26 12:13:36 -0400

    feat: translating inputs

--- solace.c
+++ solace.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -21,8 +23,14 @@ int main() {
 
   char c;
   // read 1 byte from stdin till theres nothing to read
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    // is control character
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  };
 
   return 0;
 }
