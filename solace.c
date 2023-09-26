745ab6306d9410c364c09927b3837eff47ed65c2
Selyss <99344963+Selyss@users.noreply.github.com>
2023-09-26 10:46:25 -0400

    feat: raw mode

--- solace.c
+++ solace.c
@@ -1,4 +1,28 @@
-int main()
-{
-    return 0;
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+  // read 1 byte from stdin till theres nothing to read
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    ;
+
+  return 0;
 }
