b3364e1c5bf1fd62cfbdf68b7c74009e9ff692c4
Selyss <99344963+Selyss@users.noreply.github.com>
2023-10-05 16:46:08 -0400

    Update

--- include/ui.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-void editorSetStatusMessage(const char *fmt, ...);
-void editorRefreshScreen();
-int editorSyntaxToColor(int hl);
-int editorRowCxToRx(erow *row, int cx);
-
-#endif
